f5776ca02853e5b8a48d185c1149046399e0b9bf
ARUN RAJ <s.arun.raj@gmail.com>
2019-03-06 22:06:21 -0500

    Added try/except block
    type: feat
    
    Added try/Except block to all of the requests

--- planningAPIs.py
+++ planningAPIs.py
@@ -21,11 +21,15 @@ def get_application_details():
 		Input: None
 		Output: application name
 	'''
-	response = requests.get(rest_end_point,auth=HTTPBasicAuth(uname,password),headers = headers)
-	if response.status_code == 200:
+	try:
+		response = requests.get(rest_end_point,auth=HTTPBasicAuth(uname,password),headers = headers)
+		if response.status_code == 200:
 		json_data = json.loads(response.text)
 		for d in json_data["items"]:
 			print("Application Name : ", d['name'])
+	except Exception as e:
+		print(e)
+	
 
 def get_planning_api_version():
 	'''
@@ -33,13 +37,21 @@ def get_planning_api_version():
 		Input: None
 		Output: planning api version
 	'''
-	response = requests.get(planning_url,auth=HTTPBasicAuth(uname,password),headers = headers)
-	if response.status_code == 200:
-		version_data = json.loads(response.text)
-		for v in version_data["items"]:
-			if(v['isLatest']):
-				api_version = v['version']
-	return(api_version)
+
+	try:
+		response = requests.get(planning_url,auth=HTTPBasicAuth(uname,password),headers = headers)
+		if response.status_code == 200:
+			version_data = json.loads(response.text)
+			for v in version_data["items"]:
+				if(v['isLatest']):
+					api_version = v['version']
+		return(api_version)
+	except Exception as e:
+		print(e)
+		return('ERROR')
+
+
+
 
 def get_migration_api_version():
 	'''
@@ -47,30 +59,36 @@ def get_migration_api_version():
 		Input: None
 		Output: migration api version
 	'''
-	response = requests.get(migration_url,auth=HTTPBasicAuth(uname,password),headers = headers)
-	if response.status_code == 200:
-		version_data = json.loads(response.text)
-		for v in version_data["items"]:
-			if(v['latest']):
-				api_version = v['version']
-	return(api_version)
+	
+	try:
+		response = requests.get(migration_url,auth=HTTPBasicAuth(uname,password),headers = headers)
+		if response.status_code == 200:
+			version_data = json.loads(response.text)
+			for v in version_data["items"]:
+				if(v['latest']):
+					api_version = v['version']
+		return(api_version)
+	except Exception as e:
+		print(e)
+		return('ERROR')
 
+# read file and return the details
 domain_name, uname, password, instance_url = get_login_details()
 
 planning_url =  instance_url + '/HyperionPlanning/rest/'
 migration_url = instance_url + '/interop/rest/'
 
+#get planning api version
 planning_api_version = get_planning_api_version()	
 
 rest_end_point = instance_url + '/HyperionPlanning/rest/' + planning_api_version + '/applications'
 
+#print appliation name
 get_application_details()
 
+#get migration api version
 migration_api_version = get_migration_api_version()	
 		
 print('Planning API Version : ',planning_api_version)
 print('Migration API Version : ',migration_api_version)
-
-
-
 
